1b744863e335cd383f636648c942b2301ba95c7c
hironomiu <hironomiu@gmail.com>
2022-08-02 08:05:40 +0900

    commited

---
+++ README.md
@@ -0,0 +1,6 @@
+# rust_actix_web_sqlx
+
+## Install Memo
+```
+cargo add actix-web
+```
---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use actix_web::{get, App, HttpResponse, HttpServer};
+
+#[get("/")]
+async fn index() -> Result<HttpResponse, actix_web::Error> {
+    let response_body = "hello";
+    Ok(HttpResponse::Ok().body(response_body))
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), actix_web::Error> {
+    HttpServer::new(move || App::new().service(index))
+        .bind("0.0.0.0:8686")?
+        .run()
+        .await?;
+    Ok(())
+}
