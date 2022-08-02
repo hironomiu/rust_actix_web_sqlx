a7c8881e4797c10cad489d028dde506e607702d8
hironomiu <hironomiu@gmail.com>
2022-08-02 12:19:48 +0900

    commited

--- src/databases/sqlite.rs
+++ src/databases/sqlite.rs
@@ -5,6 +5,7 @@ use sqlx::{
 
 use std::path::PathBuf;
 
+// TODO: 一旦パスは固定、どこかで引数で受ける
 pub async fn connect_sqlite_db() -> sqlx::Result<SqliteConnection> {
     let path = PathBuf::from("./sqlite_db/sample.db");
     SqliteConnection::connect_with(&SqliteConnectOptions::new().filename(path)).await
--- src/routes/sample.rs
+++ src/routes/sample.rs
@@ -4,6 +4,7 @@ use rust_actix_web_sqlx::errors;
 use rust_actix_web_sqlx::structs::{SamplePost, SampleRow};
 
 pub async fn get() -> Result<HttpResponse, errors::Error> {
+    // MEMO: ---------- MySQL ----------
     let pool = match mysql::create_mysql_connection_pool().await {
         Ok(pool) => pool,
         Err(_) => panic!("error"),
@@ -16,7 +17,22 @@ pub async fn get() -> Result<HttpResponse, errors::Error> {
         Ok(rows) => rows,
         Err(_) => panic!("error"),
     };
-    println!("{:?}", mysql_sample_rows);
+    println!("MySQL: {:?}", mysql_sample_rows);
+
+    // MEMO: ---------- sqlite ----------
+    let mut sqlite_conn = match sqlite::connect_sqlite_db().await {
+        Ok(conn) => conn,
+        Err(_) => panic!("error"),
+    };
+
+    let rows: Vec<SampleRow> = match sqlx::query_as("select id,title,body from sample")
+        .fetch_all(&mut sqlite_conn)
+        .await
+    {
+        Ok(rows) => rows,
+        Err(_) => panic!("error"),
+    };
+    println!("sqlite: {:?}", rows);
 
     Ok(HttpResponse::Ok().json(mysql_sample_rows))
 }
