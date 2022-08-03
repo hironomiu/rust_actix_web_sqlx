b6ed7b36c30e30f1918d967f7d7431bf4beb799a
hironomiu <hironomiu@gmail.com>
2022-08-03 15:31:18 +0900

    commited

--- sql/mysql/create_table.sql
+++ sql/mysql/create_table.sql
@@ -5,3 +5,15 @@ create table sample(
   created_at datetime not null default current_timestamp,
   updated_at datetime not null default current_timestamp
 );
+
+CREATE TABLE `users` (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nickname` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+);
+
--- sql/mysql/insert_data.sql
+++ sql/mysql/insert_data.sql
@@ -2,3 +2,9 @@ insert into sample(title,body) values("title1","body1");
 insert into sample(title,body) values("title2","body2");
 insert into sample(title,body) values("title3","body3");
 insert into sample(title,body) values("title4","body4");
+
+insert into users(nickname,email,password)
+values
+('太郎', 'taro@example.com', '$2b$10$wFi8RBzI3EpHt6XxqxLdLO41437B8RniV6ytM6NAACNPdFbjPj3je'),
+('花子', 'hanako@example.com' , '$2b$10$OaDQnNzHPyS4RKihI3loxuCQPogfuBz5/WYDEtvBpV0B2FTR4l0MW'),
+('Mike', 'mike@example.com'  , '$2b$10$migKeKnsy06FXJYlbWlW5eVDplNyvQDDGWmaqSHce88ceT1z3QGwm');
