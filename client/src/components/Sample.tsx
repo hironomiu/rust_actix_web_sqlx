141c242accfb6529bde6af072f835108b17f6688
hironomiu <hironomiu@gmail.com>
2022-08-10 12:47:25 +0900

    commited

--- client/src/components/Sample.tsx
+++ client/src/components/Sample.tsx
@@ -1,6 +1,7 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 const Sample = () => {
+  const [data, setData] = useState([])
   useEffect(() => {
     const fetchGetSample = async () => {
       const response = await fetch('http://localhost:8686/api/v1/sample', {
@@ -10,12 +11,19 @@ const Sample = () => {
       })
       const json = await response.json()
       console.log(json)
+      setData(json)
       return json
     }
     fetchGetSample()
-  })
+  }, [])
 
-  return <div>Sample</div>
+  return (
+    <div>
+      {data.map((d: any) => (
+        <div key={d.id}>{d.title}</div>
+      ))}
+    </div>
+  )
 }
 
 export default Sample
